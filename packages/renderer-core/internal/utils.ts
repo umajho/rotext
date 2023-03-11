93586a7acf31fd9b66c8979f771da61ed7461250
umajho <umajho@proton.me>
2023-03-11 18:24:00 +0800

    fix(renderer-core): 修复返回的 v-dom 树中会存在相同引用的 VNode
    先前尝试把 snabbdom 换成 vue 就是因为这里导致特定情况报错，现在终于
    找到原因了。

--- packages/renderer-core/internal/utils.ts
+++ packages/renderer-core/internal/utils.ts
@@ -13,6 +13,20 @@ export function intersperse<T>(arr: T[], sep: T): T[] {
 }
 
 /**
+ * 为数组 `arr` 的每两个相邻元素之间插入一个 `sepFactory` 返回的内容。
+ */
+export function intersperseWithFactory<T>(arr: T[], sepFactory: () => T): T[] {
+  const result: T[] = Array(arr.length * 2 - 1);
+  for (const [i, el] of arr.entries()) {
+    result[i * 2] = el;
+    if (i !== arr.length - 1) {
+      result[i * 2 + 1] = sepFactory();
+    }
+  }
+  return result;
+}
+
+/**
  * 返回两个字符串的公共前缀。
  */
 export function getCommonPrefix(a: string, b: string): string {
