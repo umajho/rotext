521926491fba339829fa012242d977537f7d69e6
umajho <umajho@proton.me>
2023-07-17 23:46:50 +0800

    feat(playground): 粗略为编辑器最底部垫上空白，用于优化滚动同步的体验
    当预览滚动到最下方时，编辑器可能提前滚动到头而失去于可视的最上方的同步。
    这里通过补上空白来解决这个问题。
    
    反方向理论上也可能有这个问题，等实现了反方向的同步再说。

--- playground/src/components/main/editor.tsx
+++ playground/src/components/main/editor.tsx
@@ -1,34 +1,99 @@
-import { Component, createEffect, on, Setter } from "solid-js";
+import { Component, createEffect, createSignal, on, Setter } from "solid-js";
+import { Portal } from "solid-js/web";
 import { EditorView } from "codemirror";
 
 import { createCodeMirrorEditor } from "../code-mirror-editor";
 import * as storeEditorView from "../../stores/editor-view";
 
+let nextEditorID = 1;
+
 const Editor: Component<
   { text: string; setText: Setter<string>; class?: string }
 > = (props) => {
+  const editorID = nextEditorID++;
+
+  const [blankHeightAtEnd, setBlankHeightAtEnd] = createSignal(0);
+
   const { element, view } = createCodeMirrorEditor({
     initialDoc: props.text,
     setDoc: props.setText,
-    class: props.class,
+    class: `${props.class} editor-${editorID}`,
     extensions: [EditorView.lineWrapping],
   });
 
   createEffect(on([storeEditorView.topLine], () => {
     const _view = view();
-    const _topLine = Math.min(
-      _view.state.doc.lines,
-      Math.max(storeEditorView.topLine(), 1),
+    const _topLine = clampLine(_view, storeEditorView.topLine());
+
+    const lineBlock = getLineBlock(_view, _topLine);
+    const yMargin = -lineBlock.height * (_topLine - (_topLine | 0));
+    const scrollEffect = EditorView.scrollIntoView(
+      lineBlock.from,
+      { y: "start", yMargin },
     );
 
-    const linePos = _view.state.doc.line(_topLine | 0).from;
-    const lineBlock = _view.lineBlockAt(linePos);
-    const yMargin = -lineBlock.height * (_topLine - (_topLine | 0));
-    _view.dispatch({
-      effects: [EditorView.scrollIntoView(linePos, { y: "start", yMargin })],
-    });
+    _view.dispatch({ effects: [scrollEffect] });
   }));
 
-  return element;
+  createEffect(on([storeEditorView.maxTopLineFromPreview], () => {
+    const _maxTopLineFromPreview = storeEditorView.maxTopLineFromPreview();
+    if (!_maxTopLineFromPreview) return;
+
+    const _view = view();
+    const _maxTopLine = clampLine(
+      _view,
+      storeEditorView.maxTopLineFromPreview(),
+    );
+
+    const scrollEl = findScrollElement(_view.dom);
+    if (!scrollEl) return;
+
+    const lineBlock = getLineBlock(_view, _maxTopLine);
+    const yMargin = -lineBlock.height * (_maxTopLine - (_maxTopLine | 0));
+    const maxOffsetTop = lineBlock.top + yMargin;
+
+    const lastLineBlock = getLineBlock(_view, _view.state.doc.lines);
+
+    const heightUnscrollableFromPreview = Math.max(
+      maxOffsetTop + scrollEl.offsetHeight - lastLineBlock.bottom,
+      0,
+    );
+    setBlankHeightAtEnd(heightUnscrollableFromPreview);
+  }));
+
+  return (
+    <>
+      <Portal mount={document.querySelector("head")}>
+        <style>
+          {blankHeightAtEnd() && `
+          .editor-${editorID} .cm-content::after {
+            display: block;
+            height: ${blankHeightAtEnd()}px; 
+            content: "";
+          }
+        `}
+        </style>
+      </Portal>
+      {element}
+    </>
+  );
 };
 export default Editor;
+
+function clampLine(view: EditorView, line: number) {
+  return Math.min(view.state.doc.lines, Math.max(line, 1));
+}
+
+function getLineBlock(view: EditorView, line: number) {
+  const linePos = view.state.doc.line(line | 0).from;
+  return view.lineBlockAt(linePos);
+}
+
+function findScrollElement(el: HTMLElement) {
+  el = el.parentElement;
+  while (el) {
+    if (el.scrollHeight > el.clientHeight) return el;
+    el = el.parentElement;
+  }
+  return null;
+}
