f0edb6fe8f56b9d46fa92d2a07a394a56bb3538a
umajho <umajho@proton.me>
2023-07-17 16:15:32 +0800

    feat(playground): 实现从预览到编辑器的滚动同步

--- playground/src/components/code-mirror-editor.tsx
+++ playground/src/components/code-mirror-editor.tsx
@@ -1,47 +1,34 @@
-import { Component, createEffect, onMount, Setter } from "solid-js";
+import { JSX, onMount, Setter } from "solid-js";
 
 import { basicSetup, EditorView } from "codemirror";
 import { Extension } from "@codemirror/state";
 import { oneDark } from "@codemirror/theme-one-dark";
 
-export const CodeMirrorEditor: Component<
-  {
+export function createCodeMirrorEditor(
+  props: {
     class?: string;
     extensions?: Extension[];
-    doc: string;
+    initialDoc: string;
     setDoc: Setter<string>;
-  }
-> = (
-  props,
-) => {
+  },
+): { element: JSX.Element; view: () => EditorView } {
   let parentEl: HTMLDivElement;
   let view: EditorView;
 
-  let isEditing = false;
-  createEffect(() => {
-    const doc = props.doc;
-    if (!view) return;
-    if (isEditing) {
-      isEditing = false;
-      return;
-    }
-    view.dispatch({
-      changes: { from: 0, to: view.state.doc.length, insert: doc },
-    });
-  });
-
   onMount(() => {
     const extSync = EditorView.updateListener.of((update) => {
-      isEditing = true;
       props.setDoc(update.state.doc.toString());
     });
 
     view = new EditorView({
-      doc: props.doc,
+      doc: props.initialDoc,
       extensions: [basicSetup, oneDark, extSync, ...(props.extensions ?? [])],
       parent: parentEl,
     });
   });
 
-  return <div ref={parentEl} class={`cm-parent ${props.class ?? ""}`} />;
-};
+  return {
+    element: <div ref={parentEl} class={`cm-parent ${props.class ?? ""}`} />,
+    view: () => view,
+  };
+}
