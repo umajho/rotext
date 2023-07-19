4a61d9635380821ff53ab71133b0ca38134f56f6
umajho <umajho@proton.me>
2023-07-19 21:13:11 +0800

    fix(playground): 修复无法使用 Firefox 浏览的问题
    `Intl.Segmenter` 不被 Firefox 支持。
    不支持的场合禁用相关功能（统计字数）。

--- playground/src/components/Main/EditorCard.tsx
+++ playground/src/components/Main/EditorCard.tsx
@@ -1,10 +1,15 @@
-import { Component, lazy, Setter, Suspense } from "solid-js";
+import { Component, lazy, Setter, Show, Suspense } from "solid-js";
 
 import { Badge, BadgeBar, Card, Loading } from "../ui";
 
 const Editor = lazy(() => import("./Editor"));
 
-const segmenter = new Intl.Segmenter(undefined, { granularity: "grapheme" });
+const segmenter: Intl.Segmenter | null = (() => {
+  if (window.Intl?.Segmenter) {
+    return new Intl.Segmenter(undefined, { granularity: "grapheme" });
+  }
+  return null;
+})();
 const textEncoder = new TextEncoder();
 
 const EditorCard: Component<
@@ -13,14 +18,17 @@ const EditorCard: Component<
   const editorSizeClass =
     "max-h-[25vh] lg:max-h-none lg:h-full lg:min-h-[20rem]";
 
-  const charCount = () => [...segmenter.segment(props.text)].length;
+  const charCount = () =>
+    segmenter ? [...segmenter.segment(props.text)].length : null;
   const byteCount = () => textEncoder.encode(props.text).length;
   const lineCount = () => props.text.split("\n").length;
 
   return (
     <Card class="w-full max-w-[48rem] lg:w-[36rem] lg:max-h-[80vh]">
       <BadgeBar class="pb-2">
-        <Badge>字数：{charCount()}</Badge>
+        <Show when={segmenter}>
+          <Badge>字数：{charCount()}</Badge>
+        </Show>
         <Badge>字节数：{byteCount()}</Badge>
         <Badge>行数：{lineCount()}</Badge>
       </BadgeBar>
