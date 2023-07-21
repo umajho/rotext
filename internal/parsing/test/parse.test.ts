30e5d9a3bcc5193d57705c84b8b546c791016228
umajho <umajho@proton.me>
2023-07-21 17:32:47 +0800

    fix(parsing/test): 修正在表格行内单元的空白规则修改后没有同步的测试用例

--- internal/parsing/test/parse.test.ts
+++ internal/parsing/test/parse.test.ts
@@ -681,23 +681,23 @@ describe("解析", () => {
       describe("能正确解析 “预期产物是单行” 的、“源代码使用 ‘单行格式’” 的表格", () => {
         theseCasesAreOk([
           {
-            input: "{|\n|a\n|}",
+            input: "{|\n| a\n|}",
             expected: [create.TABLE(null, [[create.TABLE$cell("D", ["a"])]])],
           },
           {
-            input: "{|\n|a||b\n|}",
+            input: "{|\n| a || b\n|}",
             expected: [create.TABLE(null, [
               [create.TABLE$cell("D", ["a"]), create.TABLE$cell("D", ["b"])],
             ])],
           },
           {
-            input: "{|\n!a!!b\n|}",
+            input: "{|\n! a !! b\n|}",
             expected: [create.TABLE(null, [
               [create.TABLE$cell("H", ["a"]), create.TABLE$cell("H", ["b"])],
             ])],
           },
           {
-            input: "{|\n!a||b\n|}",
+            input: "{|\n! a || b\n|}",
             expected: [create.TABLE(null, [
               [create.TABLE$cell("H", ["a"]), create.TABLE$cell("D", ["b"])],
             ])],
@@ -709,7 +709,7 @@ describe("解析", () => {
           //   ])],
           // },
           {
-            input: "{|\n|[`foo`]||['bar']\n|}",
+            input: "{|\n| [`foo`] || ['bar']\n|}",
             expected: [create.TABLE(null, [
               [
                 create.TABLE$cell("D", [create.code("foo")]),
@@ -722,7 +722,7 @@ describe("解析", () => {
       describe("能正确解析 “预期产物是单行” 的、“源代码使用 ‘多行格式’” 的表格", () => {
         theseCasesAreOk([
           {
-            input: "{|\n|a\n|b\n|}",
+            input: "{|\n| a\n| b\n|}",
             expected: [create.TABLE(null, [
               [create.TABLE$cell("D", ["a"]), create.TABLE$cell("D", ["b"])],
             ])],
@@ -732,7 +732,7 @@ describe("解析", () => {
       describe("能正确解析 “预期产物是单行” 的、“源代码使用 ‘两种格式混合’” 的表格", () => {
         theseCasesAreOk([
           {
-            input: "{|\n|a||b\n|c||d\n|e\n|}",
+            input: "{|\n| a || b\n| c || d\n| e\n|}",
             expected: [create.TABLE(null, [
               [
                 create.TABLE$cell("D", ["a"]),
@@ -748,7 +748,7 @@ describe("解析", () => {
       describe("能正确解析 “预期产物是多行” 的表格", () => {
         theseCasesAreOk([
           {
-            input: "{|\n!a!!b\n|-\n|c||d\n|-\n|e||f\n|}",
+            input: "{|\n! a !! b\n|-\n| c || d\n|-\n| e || f\n|}",
             expected: [create.TABLE(null, [
               [create.TABLE$cell("H", ["a"]), create.TABLE$cell("H", ["b"])],
               [create.TABLE$cell("D", ["c"]), create.TABLE$cell("D", ["d"])],
@@ -760,7 +760,7 @@ describe("解析", () => {
       describe("能正确解析源代码中 “单个单元格有多行” 的表格", () => {
         theseCasesAreOk([
           { // NOTE: MediaWiki 也是如此处理的
-            input: "{|\n|foo\nbar\n|}",
+            input: "{|\n| foo\nbar\n|}",
             expected: [
               create.TABLE(null, [[
                 create.TABLE$cell("D", ["foo", create.P(["bar"])]),
@@ -776,7 +776,7 @@ describe("解析", () => {
             ],
           },
           { // 同上
-            input: "{|\n|foo\n\n= bar =\n|}",
+            input: "{|\n| foo\n\n= bar =\n|}",
             expected: [
               create.TABLE(null, [[
                 create.TABLE$cell("D", ["foo", create.H(1, ["bar"])]),
@@ -788,7 +788,7 @@ describe("解析", () => {
       describe("能正确解析标题（caption）", () => {
         theseCasesAreOk([
           {
-            input: "{|\n|+foo\n|-\n|bar\n|}",
+            input: "{|\n|+ foo\n|-\n| bar\n|}",
             expected: [
               create.TABLE(["foo"], [[create.TABLE$cell("D", ["bar"])]]),
             ],
