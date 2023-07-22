43d1e29212f330907cd7d388229ed3485e918432
umajho <umajho@proton.me>
2023-07-22 22:55:42 +0800

    fix(playground): 在 iOS Safari 16 下由于动态工具栏导致的高度抖动

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -5,7 +5,7 @@ import Main from "./components/Main/mod";
 
 const App: Component = () => {
   return (
-    <div class="app-container min-h-screen bg-base-300">
+    <div class="app-container min-h-screen !min-h-[100dvh] bg-base-300">
       <nav class="sticky top-0 z-10 w-full p-2">
         <NavBar />
       </nav>
