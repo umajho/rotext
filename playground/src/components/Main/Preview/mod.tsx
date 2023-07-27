2aac68af600aea96236ee688934167a8bc6051c0
umajho <umajho@proton.me>
2023-07-27 14:16:24 +0800

    feat(playground): 简单地实现在预览中试投 dicexp 的功能

--- playground/src/components/Main/Preview/mod.tsx
+++ playground/src/components/Main/Preview/mod.tsx
@@ -11,6 +11,8 @@ import {
   Show,
 } from "solid-js";
 
+import { ErrorAlert } from "./ui";
+
 import {
   classModule,
   h,
@@ -132,25 +134,6 @@ const Preview: Component<
   );
 };
 export default Preview;
-
-const ErrorAlert: Component<{
-  error: Error;
-  showsStack: boolean;
-}> = (props) => {
-  return (
-    <div class="sticky top-0 z-10 max-h-32 alert alert-error shadow-lg overflow-scroll">
-      <div class="text-xs">
-        <code class="whitespace-pre">
-          {props.error.message}
-          <Show when={props.showsStack && props.error["stack"]}>
-            <hr />
-            {props.error["stack"]}
-          </Show>
-        </code>
-      </div>
-    </div>
-  );
-};
 
 function createLookupList(
   els: { scrollContainer: HTMLElement; outputContainer: HTMLElement },
