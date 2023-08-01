2275c240dc19f5037e8219588d781d79062c2d11
umajho <umajho@proton.me>
2023-08-02 01:47:13 +0800

    feat(playground): 于 ce 编辑器方案中实现更新活动行数

--- playground/src/components/Main/preview-parts/Preview/scroll-utils.ts
+++ playground/src/components/Main/preview-parts/Preview/scroll-utils.ts
@@ -1,3 +1,5 @@
+import { binarySearch } from "../../../../utils/algorithm";
+
 import {
   ElementLocationPair,
   LookupList,
@@ -183,26 +185,6 @@ export function roastLookupList(raw: LookupListRaw, rootClass: string) {
   }, null as ElementLocationPair[] | null) ?? [];
 
   return reduced;
-}
-
-function binarySearch<T>(
-  list: Array<T>,
-  predicate: (item: T, i: number) => true | "greater" | "less",
-): number | null {
-  let l = 0, h = list.length - 1;
-
-  while (true) {
-    if (h < l) return null;
-    const i = ((h - l + 1) >> 2) + l;
-    const item = list[i];
-    const p = predicate(item, i);
-    if (p === true) return i;
-    if (p === "greater") {
-      l = i + 1;
-    } else {
-      h = i - 1;
-    }
-  }
 }
 
 function getOffsetBottom(
