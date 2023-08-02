a9f46d4b1389659274ba5ab967443e1b477df062
umajho <umajho@proton.me>
2023-08-02 14:38:49 +0800

    feat(playground): 在切换到 ta 编辑器方案时清空活动行数

--- playground/src/components/Main/editor-parts/EditorByTextArea.tsx
+++ playground/src/components/Main/editor-parts/EditorByTextArea.tsx
@@ -5,13 +5,15 @@ import { Component } from "solid-js";
 import { EditorStore } from "../../../hooks/editor-store";
 
 const Editor: Component<{ store: EditorStore; class?: string }> = (props) => {
+  props.store.activeLines = null;
+
   function handleChange(ev: InputEvent) {
     props.store.text = (ev.currentTarget as HTMLTextAreaElement).value;
   }
 
   return (
     <textarea
-      class={`one-dark px-4 ${props.class} resize-none focus:!outline-none`}
+      class={`one-dark one-dark-background px-4 ${props.class} resize-none focus:!outline-none`}
       value={props.store.text}
       onInput={handleChange}
     />
