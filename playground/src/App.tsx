76841870b078d480c9d6d881179d8374ace171d4
umajho <umajho@proton.me>
2023-08-02 23:10:59 +0800

    feat(playground): 优化小屏幕上的界面

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -2,14 +2,17 @@ import type { Component } from "solid-js";
 
 import { NavBar } from "./components/layout";
 import Main from "./components/Main/mod";
+import { SUPPORTS_DVH } from "./utils/styles";
 
 const App: Component = () => {
+  const minH = SUPPORTS_DVH ? "min-h-[100dvh]" : "min-h-screen";
+
   return (
-    <div class="app-container min-h-screen !min-h-[100dvh] bg-base-300">
-      <nav class="sticky top-0 z-10 w-full p-2">
+    <div class={`app-container ${minH} bg-base-300`}>
+      <nav class="sticky top-0 z-10 w-full py-2 sm:p-2">
         <NavBar />
       </nav>
-      <div class="h-10" />
+      <div class="h-4 md:h-8" />
       <Main />
     </div>
   );
