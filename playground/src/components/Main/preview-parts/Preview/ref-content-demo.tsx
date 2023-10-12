c8bb5fed34cfa28b606896f201cf78cc5483be80
umajho <umajho@proton.me>
2023-10-12 20:37:20 +0800

    feat: 将 ref-link 挂件引用内容的渲染与挂件本身分开

---
+++ playground/src/components/Main/preview-parts/Preview/ref-content-demo.tsx
@@ -0,0 +1,90 @@
+import { Component, createSignal, JSX, Match, Switch } from "solid-js";
+import { render } from "solid-js/web";
+
+import {
+  RefAddress,
+  RefContentRenderer,
+} from "@rotext/solid-components/internal";
+
+export function createDemoRefContentRenderer(
+  opts: { proseClass: string },
+): RefContentRenderer {
+  return (el, addr, onChange, onCleanup) => {
+    const dispose = render(() => {
+      const [address, setAddress] = createSignal(addr);
+      onChange((addr) => setAddress(addr));
+      return (
+        <AddressDescription address={address()} proseClass={opts.proseClass} />
+      );
+    }, el);
+    onCleanup(dispose);
+  };
+}
+
+const AddressDescription: Component<{
+  address: RefAddress;
+  proseClass: string;
+}> = (props) => {
+  return (
+    <div class={props.proseClass}>
+      <p>这里的内容会引用自：</p>
+      <AddressDescriptionList address={props.address} />
+    </div>
+  );
+};
+
+const AddressDescriptionList = (
+  props: { address: RefAddress },
+): JSX.Element => {
+  return (
+    <Switch>
+      <Match when={props.address.type === "post_number"}>
+        {(() => {
+          const address = props.address as //
+          Extract<RefAddress, { type: "post_number" }>;
+          return (
+            <ul>
+              <li>帖号是“{address.postNumber}”的帖子。</li>
+            </ul>
+          );
+        })()}
+      </Match>
+      <Match
+        when={props.address.type === "thread_id" ||
+          props.address.type === "thread_id_sub"}
+      >
+        {(() => {
+          const address = props.address as //
+          Extract<RefAddress, { type: "thread_id" | "thread_id_sub" }>;
+          return (
+            <ul>
+              <li>
+                串号是“{address.threadID}”的串
+                {(address.type === "thread_id_sub" ||
+                      address.floorNumber !== undefined) && "的，" ||
+                  "。"}
+              </li>
+              {address.type === "thread_id_sub" && (
+                <li>
+                  ID是“{address.subThreadID}”的子级串
+                  {address.floorNumber !== undefined && "的，" || "。"}
+                </li>
+              )}
+              {address.floorNumber !== undefined &&
+                (
+                  <li>
+                    位于{address.floorNumber
+                      ? `第${address.floorNumber}层`
+                      : "串首"}的帖子。
+                  </li>
+                )}
+            </ul>
+          );
+        })()}
+      </Match>
+      <Match when={true}>
+        <p>未知地址</p>;
+      </Match>
+    </Switch>
+  );
+};
