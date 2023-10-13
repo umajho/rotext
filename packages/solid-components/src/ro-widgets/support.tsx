52e51b8f1d293ee98b7d8f6ebc64662469c92abe
umajho <umajho@proton.me>
2023-10-13 18:48:37 +0800

    fix(solid-components): 修复在没有 preflight 时挂件的水平分割线

--- packages/solid-components/src/ro-widgets/support.tsx
+++ packages/solid-components/src/ro-widgets/support.tsx
@@ -41,3 +41,17 @@ export const PinButton: Component<{
     />
   );
 };
+
+export const HorizontalRule: Component = () => (
+  <hr
+    style={{
+      width: "100%",
+      height: 0,
+      margin: 0,
+      color: "inherit",
+      "border-style": "solid",
+      "border-width": 0,
+      "border-top-width": "1px",
+    }}
+  />
+);
