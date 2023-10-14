03b787b17d49e1072bddea2c12f6a29b7aa2c5ad
umajho <umajho@proton.me>
2023-10-14 14:47:32 +0800

    build(solid-components): 取消 dev entries 的生成

--- packages/solid-components/tsup.config.ts
+++ packages/solid-components/tsup.config.ts
@@ -19,8 +19,8 @@ const preset_options: preset.PresetOptions = {
       // entries with '.tsx' extension will have `solid` export condition generated
       entry,
       name,
-      // will generate a separate development entry
-      dev_entry: true,
+      // // will generate a separate development entry
+      // dev_entry: true,
     })),
   ],
   // Set to `true` to remove all `console.*` calls and `debugger` statements in prod builds
