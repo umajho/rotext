dba2694dd7caa4114bb12a8d02ee3d4dd4217824
umajho <umajho@proton.me>
2023-10-18 14:24:07 +0800

    style(solid-components/dev): 移除不再需要的文件

--- packages/solid-components/dev/dicexp-evaluator.worker.ts
+++
@@ -1,8 +0,0 @@
-import { asScope, startWorkerServer } from "dicexp";
-import * as builtins from "@dicexp/builtins";
-
-export const scopes = {
-  "standard": asScope([builtins.operatorScope, builtins.functionScope]),
-} as const;
-
-startWorkerServer(scopes);
