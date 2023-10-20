743c7f666bec91f794745bc435bc56a66a580bfa
umajho <umajho@proton.me>
2023-10-20 23:30:53 +0800

    feat(solid-components): 创建样式提供器时，添加使用 link 元素的回退方案

--- packages/solid-components/src/support/styles.ts
+++ packages/solid-components/src/support/styles.ts
@@ -7,13 +7,25 @@ export function createStyleProviderFromCSSText(text: string): StyleProvider {
     const sheet = new CSSStyleSheet();
     sheet.replace(text);
     return sheet;
-  } catch {
+  } catch {}
+
+  try {
+    const blob = new Blob([text], { type: "text/css" });
+    // 由于样式会一直存在，就不考虑 revoke 了
+    const objectURL = URL.createObjectURL(blob);
     return () => {
-      const styleEl = document.createElement("style");
-      styleEl.appendChild(document.createTextNode(text));
-      return styleEl;
+      const linkEl = document.createElement("link");
+      linkEl.rel = "stylesheet";
+      linkEl.href = objectURL!;
+      return linkEl;
     };
-  }
+  } catch {}
+
+  return () => {
+    const styleEl = document.createElement("style");
+    styleEl.appendChild(document.createTextNode(text));
+    return styleEl;
+  };
 }
 
 export function attachStyle(shadowRoot: ShadowRoot, p: StyleProvider) {
