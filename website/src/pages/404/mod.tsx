c4aaf7560b4c697aaa6a9663124c3c9c373626fa
umajho <umajho@proton.me>
2024-07-02 17:15:34 +0800

    feat(website): 在 404 页面用 router 的 useNavigate 跳转到其他页面

--- website/src/pages/404/mod.tsx
+++ website/src/pages/404/mod.tsx
@@ -1,13 +1,18 @@
 import { Component } from "solid-js";
+import { useNavigate } from "@solidjs/router";
 
 import { Button } from "../../components/ui/mod";
 
-export default (() => (
-  <div class="flex justify-center flex-col items-center">
-    <h1 class="text-5xl font-bold">页面不存在</h1>
-    <div class="h-6" />
-    <Button type="primary" onClick={() => location.href = "/"}>
-      返回首页
-    </Button>
-  </div>
-)) satisfies Component;
+export default (() => {
+  const navigate = useNavigate();
+
+  return (
+    <div class="flex justify-center flex-col items-center">
+      <h1 class="text-5xl font-bold">页面不存在</h1>
+      <div class="h-6" />
+      <Button type="primary" onClick={() => navigate("/", { replace: true })}>
+        返回首页
+      </Button>
+    </div>
+  );
+}) satisfies Component;
