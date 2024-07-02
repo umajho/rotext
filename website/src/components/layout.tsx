6a309522e32820feaeb8af11aeeaa8cb30ea4991
umajho <umajho@proton.me>
2024-07-02 22:23:24 +0800

    feat(website): 实现导航菜单

--- website/src/components/layout.tsx
+++ website/src/components/layout.tsx
@@ -1,17 +1,47 @@
-import { Component, Suspense } from "solid-js";
-import { RouteSectionProps } from "@solidjs/router";
+import {
+  Component,
+  createEffect,
+  createSignal,
+  on,
+  Show,
+  Suspense,
+} from "solid-js";
+import { RouteSectionProps, useMatch, useNavigate } from "@solidjs/router";
 import { Portal } from "solid-js/web";
+import { createBreakpoints } from "@solid-primitives/media";
 
-import { HiSolidArrowTopRightOnSquare } from "solid-icons/hi";
+import { HiSolidArrowTopRightOnSquare, HiSolidBars3 } from "solid-icons/hi";
 
 import { SUPPORTS_DVH } from "@rotext/web-utils";
 
-import { Dropdown, DropdownItem, Loading } from "./ui/mod";
+import { Button, Dropdown, DropdownItem, Loading } from "./ui/mod";
 
 import preflight from "../preflight.css?inline";
 
+const navMenuBreakpoints = createBreakpoints({
+  "asSidebar": "768px",
+});
+
 export const Root: Component<RouteSectionProps> = (props) => {
   const height = SUPPORTS_DVH ? "h-[100dvh]" : "h-screen";
+
+  const [shouldShowNavMenu, setShouldShowNavMenu] = createSignal(true);
+  const [shouldShowNavMenuOnWideScreen, setShouldShowNavMenuOnWideScreen] =
+    createSignal(true);
+  const isDrawerOpen = () =>
+    !navMenuBreakpoints.asSidebar && shouldShowNavMenu();
+  createEffect(on([() => navMenuBreakpoints.asSidebar], () => {
+    if (navMenuBreakpoints.asSidebar) {
+      setShouldShowNavMenu(
+        shouldShowNavMenu() || shouldShowNavMenuOnWideScreen(),
+      );
+    } else {
+      setShouldShowNavMenuOnWideScreen(shouldShowNavMenu());
+      // 在 nav menu 要以抽屉的形式展现时，先将其关上。因为在访问者缩小界面时，
+      // 往往不会想让抽屉开着。
+      setShouldShowNavMenu(false);
+    }
+  }));
 
   return (
     <>
@@ -19,30 +49,64 @@ export const Root: Component<RouteSectionProps> = (props) => {
         {/* XXX: 不能放到 index.tsx 里，否则 vite dev 服务器会无限循环。 */}
         <style id="preflight">{preflight}</style>
       </Portal>
+      <Show when={isDrawerOpen()}>
+        <div
+          class={`absolute top-0 z-10 w-full ${height} bg-black opacity-50 cursor-pointer`}
+          onClick={() => setShouldShowNavMenu(false)}
+        />
+      </Show>
       <div class={`flex flex-col ${height} bg-base-300`}>
-        <nav class="sticky top-0 z-10 w-full py-2 sm:p-2">
-          <NavBar />
+        <nav class="sticky top-0 z-30 w-full py-2 sm:p-2">
+          <NavBar
+            shouldShowNavMenu={shouldShowNavMenu()}
+            setShouldShowNavMenu={setShouldShowNavMenu}
+          />
         </nav>
-        <main class="h-full">
-          <Suspense
-            fallback={
-              <div class="flex h-full justify-center items-center">
-                <Loading />
+        <div class="flex h-full">
+          <Show when={shouldShowNavMenu()}>
+            <nav
+              class={`relative justify-center ${
+                isDrawerOpen() ? "w-0" : "w-64"
+              }`}
+            >
+              <div class="absolute z-20 max-h-full h-fit w-60 m-2 overflow-y-scroll bg-base-100 rounded-box">
+                <NavMenu
+                  onClickMenuItem={() => !navMenuBreakpoints.asSidebar &&
+                    setShouldShowNavMenu(false)}
+                />
               </div>
-            }
-          >
-            {props.children}
-          </Suspense>
-        </main>
+            </nav>
+          </Show>
+          <main class="flex-1">
+            <Suspense
+              fallback={
+                <div class="flex h-full justify-center items-center">
+                  <Loading />
+                </div>
+              }
+            >
+              {props.children}
+            </Suspense>
+          </main>
+        </div>
       </div>
     </>
   );
 };
 
-const NavBar: Component = () => {
+const NavBar: Component<{
+  shouldShowNavMenu: boolean;
+  setShouldShowNavMenu: (value: boolean) => void;
+}> = (props) => {
   return (
     <div class="navbar bg-base-100 shadow-xl rounded-box">
       <div class="flex-1 flex items-center">
+        <Button
+          type={props.shouldShowNavMenu ? "neutral" : "ghost"}
+          onClick={() => props.setShouldShowNavMenu(!props.shouldShowNavMenu)}
+        >
+          <HiSolidBars3 size={24} />
+        </Button>
         <Dropdown
           summary={
             <>
@@ -81,3 +145,32 @@ const NavBar: Component = () => {
     </div>
   );
 };
+
+const NavMenu: Component<{
+  onClickMenuItem: () => boolean | void;
+}> = (props) => {
+  const navigate = useNavigate();
+  const matchPlayground = useMatch(() => "/playground");
+
+  return (
+    <ul class="menu w-full">
+      <p class="menu-title">导航</p>
+      <li>
+        <a
+          class={`${matchPlayground() ? "active" : ""}`}
+          onClick={(ev) => {
+            if (isActive(ev)) return;
+            props.onClickMenuItem();
+            navigate("/playground", { replace: true });
+          }}
+        >
+          实验场
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+function isActive(ev: { currentTarget: HTMLElement }): boolean {
+  return ev.currentTarget.classList.contains("active");
+}
--- website/src/pages/Playground/MainCard.tsx
+++ website/src/pages/Playground/MainCard.tsx
@@ -10,10 +10,10 @@ import { createPreviewParts } from "./preview-parts/mod";
 import { createEditorParts } from "./editor-parts/mod";
 
 const SIZE_OPTS = {
-  widthClass: "w-full lg:max-w-[35rem]",
+  widthClass: "w-full",
   heightClass: SUPPORTS_DVH
-    ? `h-[calc(50dvh-6rem)] sm:h-[calc(50dvh-8rem)] lg:h-[calc(100dvh-16rem)]`
-    : `h-[calc(50vh-6rem)] sm:h-[calc(50vh-8rem)] lg:h-[calc(100vh-16rem)]`,
+    ? `h-[calc(50dvh-6rem)] sm:h-[calc(50dvh-8rem)] xl:h-[calc(100dvh-16rem)]`
+    : `h-[calc(50vh-6rem)] sm:h-[calc(50vh-8rem)] xl:h-[calc(100vh-16rem)]`,
 };
 
 const DOC_INTRODUCTION = await (await fetch("docs/rotext入门.rotext")).text();
@@ -25,18 +25,21 @@ const MainCard: Component = () => {
   const { PreviewTopBar, Preview } = createPreviewParts(store, SIZE_OPTS);
 
   return (
-    <Card class="h-content" bodyClass="max-sm:px-1 max-sm:py-1">
-      <div class="grid grid-cols-1 lg:grid-cols-2">
-        <div class="max-lg:order-1">
+    <Card
+      class="h-content xl:max-w-[80rem]"
+      bodyClass="max-sm:px-1 max-sm:py-1"
+    >
+      <div class="grid grid-cols-1 xl:grid-cols-2">
+        <div class="max-xl:order-1">
           {EditorTopBar}
         </div>
-        <div class="max-lg:order-3">
+        <div class="max-xl:order-3">
           {PreviewTopBar}
         </div>
-        <div class="max-lg:order-2">
+        <div class="max-xl:order-2">
           {Editor}
         </div>
-        <div class="max-lg:order-4">
+        <div class="max-xl:order-4">
           {Preview}
         </div>
       </div>
