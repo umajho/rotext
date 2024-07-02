9325935ffcd59b17e66a913bfe389e1416b798e2
umajho <umajho@proton.me>
2024-07-02 17:06:58 +0800

    style(website): 精简代码

--- website/src/pages/Playground/mod.tsx
+++ website/src/pages/Playground/mod.tsx
@@ -2,14 +2,8 @@ import { Component } from "solid-js";
 
 import MainCard from "./MainCard";
 
-export default (() => {
-  return (
-    <div
-      class={`
-        flex justify-center flex-col items-center
-      `}
-    >
-      <MainCard />
-    </div>
-  );
-}) satisfies Component;
+export default (() => (
+  <div class="flex justify-center flex-col items-center">
+    <MainCard />
+  </div>
+)) satisfies Component;
