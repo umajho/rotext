013dc9252167c27e439aa75d4680fc80a8c3169d
umajho <umajho@proton.me>
2024-07-17 16:36:31 +0800

    docs(rust/rotext): 在 EventType 定义内部添加一些注释

--- rust/rotext/src/events.rs
+++ rust/rotext/src/events.rs
@@ -1,14 +1,21 @@
 #[derive(Debug, PartialEq, Eq)]
 #[repr(u32)]
 pub enum EventType {
+    // 在全局阶段产出，由块级阶段和行内阶段逐渐消耗。
     Unparsed = 1,
+
+    // 换行相关，皆在全局阶段产出。其中 CR 在块级阶段消耗（可能转化为 LF），LF
+    // 在块级阶段、行内阶段保留或消耗。
     CarriageReturn = 11,
     LineFeed = 12,
 
+    // 在块级阶段与行内阶段产出。
     Exit = 1001,
 
+    // 全局阶段产出，在块级阶段消耗（转化为 [EventType::Text）。
     VerbatimEscaping = 10001,
 
+    // 在块级阶段产出。
     EnterParagraph = 20001,
     /// XXX: 数字是临时的。
     ThematicBreak = 29001,
@@ -19,6 +26,7 @@ pub enum EventType {
     /// XXX: 数字是临时的。
     EnterCodeBlockContent = 29013,
 
+    // 在块级阶段与行内阶段产出。
     Text = 30001,
 }
 
