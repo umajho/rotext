9ae6b7c6ceee5c4d63c5d03802d86b34a55fa16c
umajho <umajho@proton.me>
2024-07-17 16:56:36 +0800

    feat(rust/rotext): 调整 EventType
    由于目前事件类型对应的数值尚未暴露在外，因此并非 breaking change。

--- rust/rotext/src/events.rs
+++ rust/rotext/src/events.rs
@@ -4,29 +4,27 @@ pub enum EventType {
     // 在全局阶段产出，由块级阶段和行内阶段逐渐消耗。
     Unparsed = 1,
 
+    // 在全局阶段产出，在块级阶段消耗（转化为 [EventType::Text）。
+    VerbatimEscaping = 101,
+
     // 换行相关，皆在全局阶段产出。其中 CR 在块级阶段消耗（可能转化为 LF），LF
     // 在块级阶段、行内阶段保留或消耗。
-    CarriageReturn = 11,
-    LineFeed = 12,
+    CarriageReturn = 201,
+    LineFeed = 202,
 
     // 在块级阶段与行内阶段产出。
-    Exit = 1001,
-
-    // 在全局阶段产出，在块级阶段消耗（转化为 [EventType::Text）。
-    VerbatimEscaping = 10001,
+    Text = 1001,
+    Exit = 1011,
 
     // 在块级阶段产出。
-    EnterParagraph = 20001,
-    ThematicBreak = 20011,
+    EnterParagraph = 10001,
+    ThematicBreak = 10011,
     /// XXX: 数字是临时的。
-    EnterCodeBlock = 29011,
+    EnterCodeBlock = 19011,
     /// XXX: 数字是临时的。
-    EnterCodeBlockMeta = 29012,
+    EnterCodeBlockMeta = 19012,
     /// XXX: 数字是临时的。
-    EnterCodeBlockContent = 29013,
-
-    // 在块级阶段与行内阶段产出。
-    Text = 30001,
+    EnterCodeBlockContent = 19013,
 }
 
 #[cfg(test)]
