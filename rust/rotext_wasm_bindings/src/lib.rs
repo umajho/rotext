86cf9d8be0822d1ddb4c2b988a97c3855ab987af
umajho <umajho@proton.me>
2024-07-20 03:48:47 +0800

    build(rust): 配置 dev build，并只在 dev build 中启用部分协助除错的功能

--- rust/rotext_wasm_bindings/src/lib.rs
+++ rust/rotext_wasm_bindings/src/lib.rs
@@ -7,14 +7,18 @@ pub fn parse(input: &[u8]) -> usize {
     todo!()
 }
 
+#[cfg(debug_assertions)]
 static INIT: Once = Once::new();
 
 #[wasm_bindgen]
 pub fn dev(input: &[u8]) -> String {
-    console_error_panic_hook::set_once();
-    INIT.call_once(|| {
-        console_log::init_with_level(log::Level::Debug).unwrap();
-    });
+    #[cfg(debug_assertions)]
+    {
+        console_error_panic_hook::set_once();
+        INIT.call_once(|| {
+            console_log::init_with_level(log::Level::Debug).unwrap();
+        });
+    }
 
     rotext::parse_and_render_to_html(input)
 }
