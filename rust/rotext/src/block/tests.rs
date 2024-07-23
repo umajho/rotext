839699a25ee52ee63e1f3f2f5608eb09d3e329ae
umajho <umajho@proton.me>
2024-07-23 17:43:15 +0800

    test(rust/rotext): 为列表的解析添加测试（目前有 4 例不通过）

--- rust/rotext/src/block/tests.rs
+++ rust/rotext/src/block/tests.rs
@@ -244,7 +244,7 @@ type EventCase<'a> = (EventType, Option<&'a str>);
         (EventType::Text, Some("c")),
         (EventType::Unparsed, Some("b")),
     (EventType::Exit, None)])]
-// ## 块引用
+// ## 块引用与其延续
 #[case(vec!["> foo", " > foo", ">  foo"], vec![
     (EventType::EnterBlockQuote, None),
         (EventType::EnterParagraph, None),
@@ -372,6 +372,319 @@ type EventCase<'a> = (EventType, Option<&'a str>);
     (EventType::Exit, None),
     (EventType::EnterHeading3, None),
         (EventType::Unparsed, Some("b")),
+    (EventType::Exit, None)])]
+// ## 列表
+#[case(vec!["# 1"], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("1")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec!["* 1"], vec![
+    (EventType::EnterUnorderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("1")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        # 1
+        # 2"},
+], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("1")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("2")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        * a
+        * b
+        * c"},
+    ], vec![
+    (EventType::EnterUnorderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("a")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("b")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("c")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        # 1
+
+        # 2"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("1")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None),
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("2")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec!["# # 1.1"], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterOrderedList, None),
+                (EventType::EnterListItem, None),
+                    (EventType::EnterParagraph, None),
+                        (EventType::Unparsed, Some("1.1")),
+                    (EventType::Exit, None),
+                (EventType::Exit, None),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        # # 1.1
+        # 2"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterOrderedList, None),
+                (EventType::EnterListItem, None),
+                    (EventType::EnterParagraph, None),
+                        (EventType::Unparsed, Some("1.1")),
+                    (EventType::Exit, None),
+                (EventType::Exit, None),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("2")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        # # 1.1
+        #
+        # 2"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterOrderedList, None),
+                (EventType::EnterListItem, None),
+                    (EventType::EnterParagraph, None),
+                        (EventType::Unparsed, Some("1.1")),
+                    (EventType::Exit, None),
+                (EventType::Exit, None),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("2")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        # 1
+        # # 2.1"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("1")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterOrderedList, None),
+                (EventType::EnterListItem, None),
+                    (EventType::EnterParagraph, None),
+                        (EventType::Unparsed, Some("2.1")),
+                    (EventType::Exit, None),
+                (EventType::Exit, None),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        # 1
+        #
+        # # 2.1"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("1")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterOrderedList, None),
+                (EventType::EnterListItem, None),
+                    (EventType::EnterParagraph, None),
+                        (EventType::Unparsed, Some("2.1")),
+                    (EventType::Exit, None),
+                (EventType::Exit, None),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        # 1
+        # # 2.1
+        # 3"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("1")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterOrderedList, None),
+                (EventType::EnterListItem, None),
+                    (EventType::EnterParagraph, None),
+                        (EventType::Unparsed, Some("2.1")),
+                    (EventType::Exit, None),
+                (EventType::Exit, None),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("3")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        # # 1.1
+        # 2
+        # # 3.1"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterOrderedList, None),
+                (EventType::EnterListItem, None),
+                    (EventType::EnterParagraph, None),
+                        (EventType::Unparsed, Some("1.1")),
+                    (EventType::Exit, None),
+                (EventType::Exit, None),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("2")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterOrderedList, None),
+                (EventType::EnterListItem, None),
+                    (EventType::EnterParagraph, None),
+                        (EventType::Unparsed, Some("3.1")),
+                    (EventType::Exit, None),
+                (EventType::Exit, None),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+// ### 不同种类的列表
+#[case(vec![
+    indoc!{"
+        # 1
+        * a"},
+    indoc!{"
+        # 1
+
+        * a"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("1")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None),
+    (EventType::EnterUnorderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("a")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+// ### 列表的延续
+#[case(vec![
+    indoc!{"
+        # a
+        > b"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("a")),
+                (EventType::LineBreak, None),
+                (EventType::Unparsed, Some("b")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        # a
+        >
+        > b"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("a")),
+            (EventType::Exit, None),
+            (EventType::EnterParagraph, None),
+                (EventType::Unparsed, Some("b")),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
+    (EventType::Exit, None)])]
+#[case(vec![
+    indoc!{"
+        # # 1.1
+        > # 1.2"},
+    ], vec![
+    (EventType::EnterOrderedList, None),
+        (EventType::EnterListItem, None),
+            (EventType::EnterOrderedList, None),
+                (EventType::EnterListItem, None),
+                    (EventType::EnterParagraph, None),
+                        (EventType::Unparsed, Some("1.1")),
+                    (EventType::Exit, None),
+                (EventType::Exit, None),
+                (EventType::EnterListItem, None),
+                    (EventType::EnterParagraph, None),
+                        (EventType::Unparsed, Some("1.2")),
+                    (EventType::Exit, None),
+                (EventType::Exit, None),
+            (EventType::Exit, None),
+        (EventType::Exit, None),
     (EventType::Exit, None)])]
 // ## 代码块
 #[case(vec![
