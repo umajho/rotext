442f3d3da1f979830a839030d0c2a67c473d696f
umajho <umajho@proton.me>
2024-07-27 03:29:09 +0800

    docs(rust/rotext): 注释里少打一个括号

--- rust/rotext/src/events.rs
+++ rust/rotext/src/events.rs
@@ -8,7 +8,7 @@ pub enum EventType {
     // 在全局阶段产出，由块级阶段和行内阶段逐渐消耗。
     Unparsed = 1,
 
-    // 在全局阶段产出，在块级阶段消耗（转化为 [EventType::Text）。
+    // 在全局阶段产出，在块级阶段消耗（转化为 [EventType::Text]）。
     VerbatimEscaping = 101,
 
     // 换行相关，皆在全局阶段产出。其中 CR 在块级阶段消耗（可能转化为 LF），LF
