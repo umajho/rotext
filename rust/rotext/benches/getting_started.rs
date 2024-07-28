188d9b7c27c624eaa5d9057f2d3a6740357820fd
umajho <umajho@proton.me>
2024-07-28 18:58:17 +0800

    test(rust/rotext): 调整 bench

--- rust/rotext/benches/getting_started.rs
+++ rust/rotext/benches/getting_started.rs
@@ -7,7 +7,7 @@ fn main() {
 static CONTENT: LazyLock<String> = LazyLock::new(|| read_doc("rotext入门-new.rotext"));
 
 #[divan::bench(sample_size = 10)]
-fn parsing_getting_started(bencher: divan::Bencher) {
+fn parsing(bencher: divan::Bencher) {
     let file_content = CONTENT.clone();
 
     bencher.bench(|| {
@@ -16,11 +16,15 @@ fn parsing_getting_started(bencher: divan::Bencher) {
 }
 
 #[divan::bench(sample_size = 10)]
-fn rendering_getting_started_using_vec_u8(bencher: divan::Bencher) {
+fn rendering(bencher: divan::Bencher) {
     let file_content = CONTENT.clone();
 
     bencher
-        .with_inputs(|| rotext::parse(file_content.as_bytes()))
+        .with_inputs(|| {
+            rotext::parse(file_content.as_bytes())
+                .collect::<Vec<_>>()
+                .into_iter()
+        })
         .bench_refs(|events| {
             let renderer = rotext::HtmlRenderer::new(
                 file_content.as_bytes(),
@@ -32,6 +36,24 @@ fn rendering_getting_started_using_vec_u8(bencher: divan::Bencher) {
             );
             renderer.render(events);
         })
+}
+
+#[divan::bench(sample_size = 10)]
+fn parsing_and_rendering(bencher: divan::Bencher) {
+    let file_content = CONTENT.clone();
+
+    bencher.bench(|| {
+        let events = rotext::parse(file_content.as_bytes());
+        let renderer = rotext::HtmlRenderer::new(
+            file_content.as_bytes(),
+            rotext::NewHtmlRendererOptoins {
+                initial_output_string_capacity: file_content.len() * 3,
+                #[cfg(feature = "block-id")]
+                with_block_id: true,
+            },
+        );
+        renderer.render(events);
+    })
 }
 
 fn read_doc(name: &'static str) -> String {
