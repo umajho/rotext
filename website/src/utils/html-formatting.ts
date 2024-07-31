8e563f23f7fff2a158949430ab290342e7aed6df
umajho <umajho@proton.me>
2024-07-31 16:21:14 +0800

    fix(website): 更新过时的 HTML 格式化代码
    不再将 `<hr>` `<br>` 格式化为自闭合的形式。

--- website/src/utils/html-formatting.ts
+++ website/src/utils/html-formatting.ts
@@ -2,8 +2,5 @@ export function formatHTML(
   input: string,
   opts: { formatter: (input: string) => string },
 ) {
-  return opts.formatter(input)
-    // workarounds
-    .replace(/<br>/g, "<br />")
-    .replace(/<hr>/g, "<hr />");
+  return opts.formatter(input);
 }
