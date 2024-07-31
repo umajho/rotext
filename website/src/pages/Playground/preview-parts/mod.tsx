cb171c5039466b2c21491bd013a50d7699b75890
umajho <umajho@proton.me>
2024-08-01 03:59:36 +0800

    fix(website): workaround 在实验场切换 rotext 处理器后 SolidJS 抛出异常

--- website/src/pages/Playground/preview-parts/mod.tsx
+++ website/src/pages/Playground/preview-parts/mod.tsx
@@ -1,5 +1,6 @@
 import {
   createEffect,
+  createMemo,
   createSignal,
   Index,
   JSX,
@@ -46,14 +47,15 @@ export function createPreviewParts(
   const [processResult, setProcessResult] = createSignal<
     RotextProcessResult | null
   >(null);
+  const [isProcessorProviderReady, setIsProcessorProviderReady] = createSignal(
+    false,
+  );
   createEffect(
     on(
       [() => rotextProcessors.currentProvider, () => store.text],
       ([processorProvider, text]) => {
-        if (!processorProvider) {
-          setProcessResult(null);
-          return;
-        }
+        setIsProcessorProviderReady(!!processorProvider);
+        if (!processorProvider) return;
         const processor = processorProvider();
 
         setProcessResult(
@@ -104,8 +106,24 @@ export function createPreviewParts(
     ),
     Preview: (
       <div class={`flex flex-col items-center ${opts.heightClass}`}>
+        {
+          /*
+            原先本来只有一个接收 `processResult()` 的 `<Show />`，且
+            `processResult()` 在初始情况之外是可以被设置回 null 的。（用来代表正
+            在切换 rotext 处理器，fallback 到加载指示器。）
+
+            原本好好的，不知怎么回事，不知什么时候起，突然就会在
+            `setProcessResult(null)` 时报错称 “Attempting to access a stale
+            value from <Show> that could possibly be undefined.”
+
+            目前只好增加一层 `<Show />` 作为 workaround。
+
+            先前在 `<Preview />` 里的错误警告处也遇到了这个问题，看来是出于相同
+            的原因。
+          */
+        }
         <Show
-          when={processResult()}
+          when={isProcessorProviderReady}
           fallback={
             <div
               class={`flex justify-center items-center h-full ${opts.widthClass}`}
@@ -114,38 +132,42 @@ export function createPreviewParts(
             </div>
           }
         >
-          {(processResult) => (
-            <>
-              <Preview
-                store={store}
-                processResult={processResult()}
-                class={`${opts.widthClass} px-4`}
-                hidden={currentTab()[0] !== "preview"}
-              />
-              <Switch>
-                <Match
-                  when={currentTab()[0] === "html" && processResult().html}
-                >
-                  {
-                    /*
+          <Show
+            when={processResult()}
+          >
+            {(processResult) => (
+              <>
+                <Preview
+                  store={store}
+                  processResult={processResult()}
+                  class={`${opts.widthClass} px-4`}
+                  hidden={currentTab()[0] !== "preview"}
+                />
+                <Switch>
+                  <Match
+                    when={currentTab()[0] === "html" && processResult().html}
+                  >
+                    {
+                      /*
                       由于不明原因，pre 默认的 `whitespace: pre` 会导致其内容与
                       编辑器部分重叠起来，所以手动设为 `whitespace: pre-wrap`。
                     */
-                  }
-                  <pre class="overflow-scroll whitespace-pre-wrap">
+                    }
+                    <pre class="overflow-scroll whitespace-pre-wrap">
                     <code>
                       {pretty(processResult().html!)}
                     </code>
-                  </pre>
-                </Match>
-                <Match when={currentTab()[0] === "extra"}>
-                  <pre class="overflow-scroll whitespace-pre-wrap">
+                    </pre>
+                  </Match>
+                  <Match when={currentTab()[0] === "extra"}>
+                    <pre class="overflow-scroll whitespace-pre-wrap">
                     {processResult().extraInfos[(currentTab() as Extract<Tab, {0:"extra"}>)[1]]?.content}
-                  </pre>
-                </Match>
-              </Switch>
-            </>
-          )}
+                    </pre>
+                  </Match>
+                </Switch>
+              </>
+            )}
+          </Show>
         </Show>
       </div>
     ),
