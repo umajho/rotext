fad5e1b21456d3112a2869dd1a8570b14ceac25b
umajho <umajho@proton.me>
2024-08-03 15:48:32 +0800

    feat(website): 先将实验场默认加载的示例改回旧版，因为新式处理器还不成熟

--- website/src/pages/Playground/MainCard.tsx
+++ website/src/pages/Playground/MainCard.tsx
@@ -21,7 +21,7 @@ const SIZE_OPTS = {
 };
 
 const MainCard: Component = () => {
-  const editorStore = createEditorStore(examples["入门"]);
+  const editorStore = createEditorStore(examples["入门-legacy"]);
   const editorPartStore = createEditorPartStore();
   const previewPartStore = createPreviewPartStore();
 
