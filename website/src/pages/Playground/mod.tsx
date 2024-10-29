12d69420f089500035701564ef14be7b7e73ff9b
umajho <umajho@proton.me>
2024-10-29 22:07:08 +0800

    fix(website): 实验场的小视觉问题（由最近某次提交引入）

--- website/src/pages/Playground/mod.tsx
+++ website/src/pages/Playground/mod.tsx
@@ -3,7 +3,7 @@ import { Component } from "solid-js";
 import MainCard from "./MainCard";
 
 export default (() => (
-  <div class="w-full h-full pr-2">
+  <div class="w-full h-full xl:pr-2">
     <MainCard />
   </div>
 )) satisfies Component;
