960b79fed46aafd107008b72265b1d459690d083
umajho <umajho@proton.me>
2024-11-02 17:29:34 +0800

    chore: 去掉文档内部链接中不必要的部分

---
+++ website/scripts/single-use/20241102-rewrite-address-2.ts
@@ -0,0 +1,31 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const FOLDER_PATH = path.join(__dirname, "../../../docs/语法参考");
+
+async function main() {
+  const pageNames = (await fs.readdir(FOLDER_PATH, { recursive: true }))
+    .filter((p) =>
+      path.extname(p) === ".md" && path.basename(p) !== "navigation.md"
+    );
+  for (const name of pageNames) {
+    const p = path.join(FOLDER_PATH, name);
+    console.log(name);
+    await replaceInFile(p, /(.*)\.md$/.exec(name)![1]!);
+  }
+}
+
+async function replaceInFile(p: string, name: string) {
+  let content = await fs.readFile(p, "utf8");
+  content = content.replace(/\[\[(.*?)\]\]/g, (_, ...[addr]: [string]) => {
+    let [actual, shown] = addr.split("|", 2) as [string, string | undefined];
+    if (actual === `${name}#${shown}`) {
+      console.log("x", actual);
+      return `[[#${shown}|${shown}]]`;
+    }
+    return `[[${addr}]]`;
+  });
+  await fs.writeFile(p, content);
+}
+
+main();
